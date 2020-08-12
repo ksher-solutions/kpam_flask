eaeb62f7674c4b29fd3c982e46a692a435ae9982
Austin (Xu) Wang <xu4wang@gmail.com>
2020-08-12 13:16:16 +0700

    module skeloton

---
+++ kpam/kpam.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#
+#
+#  The module implementation goes here
+# 
+#  Start an HTTP server, expose RESTFUL APIs for all kinds of Ksher Services.
+#  If required, pull results from Ksher Services and send update to the Application by callback/notification
+#  Manage the status of the transaction.
+#
+ 
+pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kpam',
+    version='0.0.1',
+    packages=find_packages(include=['kpam', 'kpam.*']),
+    install_requires=[
+        'rsa'
+    ]
+)
